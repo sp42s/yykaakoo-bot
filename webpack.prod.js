e4201c4288cddc33c9a59aae914d91dcdee4e90f
Makotro <leri6@hotmail.com>
2017-12-17 02:30:00 +0200

    Changed uglift plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglify-es-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
     plugins: [
