6c47f3e5d3bbaabce3c4990e572d6ef233690b74
Makotro <leri6@hotmail.com>
2017-12-17 02:40:57 +0200

    changes to params checking and apikey usage

--- src/js/commands/wowlogs.js
+++ src/js/commands/wowlogs.js
@@ -1,11 +1,14 @@
-
+"use strict"
 import axios from 'axios'
 import moment from 'moment'
-import warcraftlogsKey from '../../../config/auth'
+import auth from '../../../config/auth'
+import * as Promise from 'bluebird'
+
+
 
 export default class WowLogs {
     static async handleMessage(params) {
-        if(params[0] === 'help') return 'Voit lisätä komennon perään parametrejä esim: guild=ryöstöretki realm=darksorrow region=eu'
+        if (params && params[0] === 'help') return 'Voit lisätä komennon perään parametrejä esim: guild=ryöstöretki realm=darksorrow region=eu'
         let latestRaid = await WowLogs.getLatestRaid(params)
         let msg = `Viimeisin raidi "${latestRaid.title}" (${moment(latestRaid.start).locale('fi').format('LT')}-${moment(latestRaid.end).locale('fi').format('LT')} ${moment(latestRaid.start).locale('fi').format('L')}) löytyy osoitteesta: https://www.warcraftlogs.com/reports/${latestRaid.id}`
         return msg
@@ -18,31 +21,31 @@ export default class WowLogs {
                 param.split("=")[1]
             ]
             return p;
-        }).reduce(function(prev,curr) {
+        }).reduce(function (prev, curr) {
             prev[curr[0]] = curr[1]
             return prev
-        },{})
+        }, {})
         return parsedParams
     }
 
-	static getLatestRaid(params) {
-		return new Promise((resolve, reject) => {
+    static getLatestRaid(params) {
+        return new Promise((resolve, reject) => {
             let query
-            if (params.length > 0) {
+            if (params && params.length > 0) {
                 query = WowLogs.parseParams(params);
-                if (!query.realm) query.realm ='darksorrow'
+                if (!query.realm) query.realm = 'darksorrow'
                 if (!query.region) query.region = 'eu'
-			}
-			else {
+            }
+            else {
                 query = {
                     guild: 'ryöstöretki',
                     realm: 'darksorrow',
-                    region:  'EU'
+                    region: 'EU'
                 }
             }
             if (query.guild && query.realm && query.region) {
-                axios.get(`https://www.warcraftlogs.com:443/v1/reports/guild/${encodeURIComponent(query.guild)}/${query.realm}/${query.region}?api_key=${warcraftlogsKey}`)
-                    .then( (res) => {
+                axios.get(`https://www.warcraftlogs.com:443/v1/reports/guild/${encodeURIComponent(query.guild)}/${query.realm}/${query.region}?api_key=${auth.warcraftlogsKey}`)
+                    .then((res) => {
                         let latestRaid = res.data.reduce((prev, curr) => {
                             return ((prev.end > curr.end)) ? prev : curr
                         })
@@ -54,6 +57,6 @@ export default class WowLogs {
             } else {
                 reject("getLatestRaid: invalid query")
             }
-		});
-	}
+        });
+    }
 }
